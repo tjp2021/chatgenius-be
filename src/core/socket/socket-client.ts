550d8f072c631e0034ba3457d0f22a93a26fe61a
tyjoo <tyjoo21@gmail.com>
2025-01-10 16:26:29 -0700

    ssocket update types

--- src/core/socket/socket-client.ts
+++ src/core/socket/socket-client.ts
@@ -18,6 +18,31 @@ export interface SocketState {
   error: Error | null;
 }
 
+export type SocketEvent = 
+  | 'connect'
+  | 'disconnect'
+  | 'error'
+  | 'connection:starting'
+  | 'connection:ready'
+  | 'connection:error'
+  | 'reconnect'
+  | 'reconnect_attempt'
+  | 'reconnect_failed'
+  | 'stateChange';
+
+export type SocketEventData = {
+  'connect': void;
+  'disconnect': void;
+  'error': Error;
+  'connection:starting': void;
+  'connection:ready': void;
+  'connection:error': Error;
+  'reconnect': number;
+  'reconnect_attempt': number;
+  'reconnect_failed': void;
+  'stateChange': SocketState;
+}
+
 export class SocketClient extends EventEmitter {
   private socket: Socket | null = null;
   private reconnectAttempts = 0;
@@ -72,7 +97,7 @@ export class SocketClient extends EventEmitter {
         isConnecting: false,
         error: null 
       });
-      this.emit('connect');
+      this.emitEvent('connect');
     });
 
     this.socket.on('disconnect', () => {
@@ -81,10 +106,10 @@ export class SocketClient extends EventEmitter {
         isAuthenticated: false,
         isReady: false
       });
-      this.emit('disconnect');
+      this.emitEvent('disconnect');
     });
 
-    this.socket.on('connect_error', (error) => {
+    this.socket.on('connect_error', (error: Error) => {
       this.updateState({ 
         isConnected: false,
         isConnecting: false,
@@ -97,40 +122,40 @@ export class SocketClient extends EventEmitter {
         this.config.onConnectionError?.(error);
       }
 
-      this.emit('error', error);
+      this.emitEvent('error', error);
     });
 
     // Custom connection state events
     this.socket.on('connection:starting', () => {
       this.updateState({ isAuthenticated: true });
-      this.emit('connection:starting');
+      this.emitEvent('connection:starting');
     });
 
     this.socket.on('connection:ready', () => {
       this.updateState({ isReady: true });
-      this.emit('connection:ready');
+      this.emitEvent('connection:ready');
     });
 
-    this.socket.on('connection:error', (error) => {
+    this.socket.on('connection:error', (error: { message: string }) => {
       this.updateState({ 
         error: new Error(error.message),
         isAuthenticated: false,
         isReady: false
       });
-      this.emit('connection:error', error);
+      this.emitEvent('connection:error', new Error(error.message));
     });
 
     // Reconnection events
-    this.socket.on('reconnect', (attemptNumber) => {
+    this.socket.on('reconnect', (attemptNumber: number) => {
       this.reconnectAttempts = 0;
       this.config.onReconnect?.();
-      this.emit('reconnect', attemptNumber);
+      this.emitEvent('reconnect', attemptNumber);
     });
 
     this.socket.on('reconnect_attempt', () => {
       this.reconnectAttempts++;
       this.updateState({ isConnecting: true });
-      this.emit('reconnect_attempt', this.reconnectAttempts);
+      this.emitEvent('reconnect_attempt', this.reconnectAttempts);
     });
 
     this.socket.on('reconnect_failed', () => {
@@ -138,13 +163,17 @@ export class SocketClient extends EventEmitter {
         isConnecting: false,
         error: new Error('Reconnection failed')
       });
-      this.emit('reconnect_failed');
+      this.emitEvent('reconnect_failed');
     });
   }
 
   private updateState(newState: Partial<SocketState>) {
     this.state = { ...this.state, ...newState };
-    this.emit('stateChange', this.state);
+    this.emitEvent('stateChange', this.state);
+  }
+
+  private emitEvent<E extends SocketEvent>(event: E, ...args: SocketEventData[E] extends void ? [] : [SocketEventData[E]]) {
+    super.emit(event, ...(args as any[]));
   }
 
   // Public methods
@@ -164,21 +193,35 @@ export class SocketClient extends EventEmitter {
     this.socket?.connect();
   }
 
-  public emit(event: string, ...args: any[]): boolean {
-    if (!this.isConnected()) {
-      this.emit('error', new Error('Socket not connected'));
-      return false;
-    }
-    return this.socket?.emit(event, ...args) || false;
+  public sendEvent<T = any>(event: string, data?: T): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (!this.isConnected()) {
+        reject(new Error('Socket not connected'));
+        return;
+      }
+
+      const timeout = setTimeout(() => {
+        reject(new Error('Event send timeout'));
+      }, this.CONNECT_TIMEOUT);
+
+      this.socket?.emit(event, data, (error?: Error) => {
+        clearTimeout(timeout);
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 
-  public on(event: string, listener: (...args: any[]) => void): this {
-    this.socket?.on(event, listener);
+  public on<E extends SocketEvent>(event: E, listener: (data: SocketEventData[E]) => void): this {
+    super.on(event, listener as any);
     return this;
   }
 
-  public off(event: string, listener: (...args: any[]) => void): this {
-    this.socket?.off(event, listener);
+  public off<E extends SocketEvent>(event: E, listener: (data: SocketEventData[E]) => void): this {
+    super.off(event, listener as any);
     return this;
   }
 
